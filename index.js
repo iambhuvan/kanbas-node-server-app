013cbbb9edcc4b1748492e811361fe78448b63fd
Bhuvan <bhuvan@Bhuvans-MacBook-Air.local>
2024-12-06 12:23:37 -0500

    A6

--- index.js
+++ index.js
@@ -1,49 +1,45 @@
 import express from 'express';
-import Hello from './hello.js';
+import mongoose from "mongoose";
+import Hello from "./hello.js";
 import Lab5 from "./Lab5/index.js";
+import cors from "cors";
+import session from "express-session";
 import UserRoutes from "./Kanbas/Users/routes.js";
 import CourseRoutes from "./Kanbas/Courses/routes.js";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
-import cors from "cors";
-import session from "express-session";
-import AssignmentRoutes from './Kanbas/Assignments/routes.js';
 import EnrollmentRoutes from './Kanbas/Enrollments/routes.js';
+import AssignmentRoutes from './Kanbas/Assignments/routes.js';
 import "dotenv/config";
 
-const app = express();
-app.use(
-    cors({
-      credentials: true,
-      origin: process.env.NETLIFY_URL || "http://localhost:3000",
-    })
-);
 
+const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://localhost:27017/Kanbas"
+mongoose.connect(CONNECTION_STRING);
+
+const app = express();
+app.use(cors({
+    credentials: true,
+    origin: process.env.NETLIFY_URL || "http://localhost:3000",
+}));
 const sessionOptions = {
     secret: process.env.SESSION_SECRET || "kanbas",
     resave: false,
     saveUninitialized: false,
 };
-
 if (process.env.NODE_ENV !== "development") {
     sessionOptions.proxy = true;
     sessionOptions.cookie = {
-      sameSite: "none",
-      secure: true,
-      domain: process.env.NODE_SERVER_DOMAIN,
+        sameSite: "none",
+        secure: true,
+        domain: process.env.NODE_SERVER_DOMAIN,
     };
 }
-  
-app.use(
-    session(sessionOptions)
-);
-   
+app.use(session(sessionOptions));
 app.use(express.json());
 UserRoutes(app);
 CourseRoutes(app);
-Lab5(app);
-Hello(app);
 ModuleRoutes(app);
 EnrollmentRoutes(app);
 AssignmentRoutes(app);
-
-app.listen(process.env.PORT || 4000)
+Lab5(app);
+Hello(app);
+app.listen(process.env.PORT || 4000);
